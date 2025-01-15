1a6cd508cb43fbff74be7168d46aa799de500184
EragonKing2020 <Eragonking2020@gmail.com>
2025-01-15 11:51:24 +0100

    Test upload file

--- backend/src/main/java/com/musiqueetsi/backend/controller/MusicController.java
+++ backend/src/main/java/com/musiqueetsi/backend/controller/MusicController.java
@@ -1,16 +1,24 @@
 package com.musiqueetsi.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.musiqueetsi.backend.MusiqueLogique;
 import com.musiqueetsi.backend.model.Music;
-import com.musiqueetsi.backend.service.LoginService;
+//import com.musiqueetsi.backend.service.LoginService;
 import com.musiqueetsi.backend.service.MusicService;
+import com.musiqueetsi.model.MusiqueProperties;
 
 @RestController
 @RequestMapping("/music")
+@CrossOrigin("http://localhost:8080")
 public class MusicController {
 	
 	 @Autowired
@@ -19,5 +27,16 @@ public class MusicController {
 	 @GetMapping("/music_available")
 	 public Music[] getMusicAvailable() {
 		 return null;
+	 }
+	 
+	 
+	 @PostMapping("")
+	 public String postMusique(@RequestParam("file") MultipartFile file,
+			 					//@RequestParam("properties") MusiqueProperties properties,
+			 					RedirectAttributes redirectAttributes) {
+		 MusiqueProperties properties = new MusiqueProperties(0, "name", "auteur", "description", "15/01/2025", "agiles/", "30/01/2025");
+		 String message = MusiqueLogique.writeMusique(file, properties);
+		 redirectAttributes.addFlashAttribute("message", message);
+		 return "redirect:/";
 	 }
 }
